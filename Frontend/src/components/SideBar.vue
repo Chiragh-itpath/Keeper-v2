415b05be50a01115746b41b9783d3d984ee4c36f
chirag <chiragh.itpath@gmail.com>
2023-10-27 15:53:43 +0530

    sidebar changes: -> added navigration for contact page -> wrap tags in list group

--- Frontend/src/components/SideBar.vue
+++ Frontend/src/components/SideBar.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { RouterEnum } from '@/Models/enum'
-import { Uitlity, ProjectStore, KeepStore } from '@/stores'
 import { storeToRefs } from 'pinia'
 import { useRoute, useRouter } from 'vue-router'
+import { Uitlity, ProjectStore, KeepStore } from '@/stores'
+
 const { SideBarIsVisible } = storeToRefs(Uitlity())
 const { ProjectTags } = storeToRefs(ProjectStore())
 const { keepTags } = storeToRefs(KeepStore())
@@ -24,28 +25,39 @@ const navigate = (name: string) => {
                 @click="navigate(RouterEnum.PROJECT)">
                 <span class="mx-2">All Projects</span>
             </v-list-item>
-            <div
-                v-if="route.name == RouterEnum.PROJECT || route.name == RouterEnum.SHARED || route.name == RouterEnum.PROJECT_BY_TAG">
-                <v-list-item role="button" prepend-icon="mdi-folder-account" class="my-2 sweep-to-right" color="primary"
-                    :class="route.name == RouterEnum.SHARED ? 'bg-blue-grey-lighten-3 text-white' : 'text-black'"
-                    @click="navigate(RouterEnum.SHARED)">
-                    <span class="mx-2">Shared</span>
-                </v-list-item>
-                <v-divider></v-divider>
-                <v-list-item role="button" v-for="(tag, index) in ProjectTags" :key="index"
-                    @click="filterByTag(RouterEnum.PROJECT_BY_TAG, tag)" prepend-icon="mdi-tag" class="sweep-to-right my-2"
-                    :class="route.params.tag == tag ? 'bg-blue-grey-lighten-3 text-white' : 'text-black'">
-                    <span class="mx-2">{{ tag }}</span>
-                </v-list-item>
-            </div>
-            <div v-else-if="route.name == RouterEnum.KEEP || route.name == RouterEnum.KEEP_BY_TAG">
-                <v-divider></v-divider>
-                <v-list-item role="button" v-for="(tag, index) in keepTags" :key="index"
-                    @click="filterByTag(RouterEnum.KEEP_BY_TAG, tag)" prepend-icon="mdi-tag" class="sweep-to-right my-2"
-                    :class="route.params.tag == tag ? 'bg-blue-grey-lighten-3 text-white' : 'text-black'">
-                    <span class="mx-2">{{ tag }}</span>
-                </v-list-item>
-            </div>
+
+            <v-list-item
+                v-if="route.name == RouterEnum.PROJECT || route.name == RouterEnum.SHARED || route.name == RouterEnum.PROJECT_BY_TAG"
+                role="button" prepend-icon="mdi-folder-account" class="my-2 sweep-to-right" color="primary"
+                :class="route.name == RouterEnum.SHARED ? 'bg-blue-grey-lighten-3 text-white' : 'text-black'"
+                @click="navigate(RouterEnum.SHARED)">
+                <span class="mx-2">Shared</span>
+            </v-list-item>
+            <v-list-item role="button" prepend-icon="mdi-contacts" class="my-2 sweep-to-right" color="primary">
+                <span class="mx-2">Contact</span>
+            </v-list-item>
+            <v-divider></v-divider>
+            <v-list-group value="tags" prepend-icon="mdi-tag">
+                <template v-slot:activator="{ props }">
+                    <v-list-item v-bind="props" class="sweep-to-right my-2">Tags</v-list-item>
+                </template>
+                <div
+                    v-if="route.name == RouterEnum.PROJECT || route.name == RouterEnum.SHARED || route.name == RouterEnum.PROJECT_BY_TAG">
+                    <v-list-item role="button" v-for="(tag, index) in ProjectTags" :key="index"
+                        @click="filterByTag(RouterEnum.PROJECT_BY_TAG, tag)" prepend-icon="mdi-tag"
+                        class="sweep-to-right my-2"
+                        :class="route.params.tag == tag ? 'bg-blue-grey-lighten-3 text-white' : 'text-black'">
+                        <span class="mx-2">{{ tag }}</span>
+                    </v-list-item>
+                </div>
+                <div v-else-if="route.name == RouterEnum.KEEP || route.name == RouterEnum.KEEP_BY_TAG">
+                    <v-list-item role="button" v-for="(tag, index) in keepTags" :key="index"
+                        @click="filterByTag(RouterEnum.KEEP_BY_TAG, tag)" prepend-icon="mdi-tag" class="sweep-to-right my-2"
+                        :class="route.params.tag == tag ? 'bg-blue-grey-lighten-3 text-white' : 'text-black'">
+                        <span class="mx-2">{{ tag }}</span>
+                    </v-list-item>
+                </div>
+            </v-list-group>
         </v-list>
     </v-navigation-drawer>
 </template>
@@ -64,5 +76,9 @@ const navigate = (name: string) => {
 
 .v-list-item__prepend i {
     color: #26A69A;
+}
+
+.v-list-group {
+    --prepend-width: 10px !important;
 }
 </style>
