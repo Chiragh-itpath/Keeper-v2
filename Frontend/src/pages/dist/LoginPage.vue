a02976c9613eb1c0011657c3e70076aeeeaa9278
chirag hargun <125254659+Chiragh-itpath@users.noreply.github.com>
2023-12-14 10:30:07 +0530

    Update LoginPage.vue
    remove default account credentials

--- Frontend/src/pages/dist/LoginPage.vue
+++ Frontend/src/pages/dist/LoginPage.vue
@@ -10,8 +10,8 @@ const { Loading, errors } = storeToRefs(GlobalStore())
 
 const form = ref()
 const loginForm = reactive<ILogin>({
-    email: 'keeper@yopmail.com',
-    password: 'admin123'
+    email: '',
+    password: ''
 })
 
 const login = async (): Promise<void> => {
